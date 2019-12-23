1a8e7634c3dbf6fe7e8dbd355a35686a4cf16318
tom-klimovski-kappa <tom.klimovski@kappadata.io>
2019-12-23 14:43:15 -0500

    working, ready for refactoring

--- resolver.go
+++ resolver.go
@@ -4,46 +4,55 @@ package graphql
 
 import (
 	"context"
+	"errors"
+	"github.com/tom-klimovski-kappa/graphql/models"
 ) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 
 var meetups = []*models.Meetup{
 	{
-		ID: "1",
-		Name: "A Meetup",
+		ID:          "1",
+		Name:        "A Meetup",
 		Description: "A description",
-		UserID: "1",
+		UserID:      "1",
 	},
 	{
-		ID: "2",
-		Name: "A Second Meetup",
+		ID:          "2",
+		Name:        "A Second Meetup",
 		Description: "A second description",
-		UserID: "2",
+		UserID:      "2",
 	},
 }
 
 var users = []*models.User{
 	{
-		ID: "1",
+		ID:       "1",
 		Username: "tom",
-		Email: "bob@gmail.com",
+		Email:    "bob@gmail.com",
 	},
 	{
-		ID: "2",
+		ID:       "2",
 		Username: "alice",
-		Email: "alice@gmail.com",
+		Email:    "alice@gmail.com",
 	},
 }
 
+// Resolver is a comment here
 type Resolver struct{}
 
 type queryResolver struct{ *Resolver }
 
-type mutationResolver struct { *Resolver }
+type mutationResolver struct{ *Resolver }
 
-type meetupResolver struct { *Resolver }
+type meetupResolver struct{ *Resolver }
 
-type userResolver struct { *Resolver }
+type userResolver struct{ *Resolver }
 
+//Query func for resolver struct
+func (r *Resolver) Query() QueryResolver {
+	return &queryResolver{Resolver: r}
+}
+
+//Mutation should have stuff here
 func (r *Resolver) Mutation() MutationResolver {
 	return &mutationResolver{Resolver: r}
 }
@@ -57,9 +66,41 @@ func (m *mutationResolver) CreateMeetup(ctx context.Context, input NewMeetup) (*
 }
 
 func (m *meetupResolver) User(ctx context.Context, obj *models.Meetup) (*models.User, error) {
-	panic("not implemented")
+	user := new(models.User)
+
+	for _, u := range users {
+		if u.ID == obj.UserID {
+			user = u
+			break
+		}
+	}
+
+	if user == nil {
+		return nil, errors.New("user id does not exist")
+	}
+
+	return user, nil
 }
 
-func (u *userResolver) Meetups(ctx context.Context, obj *models.Meetup) (*models.User, error){
-	return nil, nil
+func (u *userResolver) Meetups(ctx context.Context, obj *models.User) ([]*models.Meetup, error) {
+	var m []*models.Meetup
+
+	for _, meetup := range meetups {
+		if meetup.UserID == obj.ID {
+			m = append(m, meetup)
+		}
+	}
+
+	return m, nil
 }
+
+//Meetup resolver
+func (r *Resolver) Meetup() MeetupResolver {
+	return &meetupResolver{ Resolver: r}
+}
+
+//User resolver
+func (r *Resolver) User() UserResolver {
+	return &userResolver{ Resolver: r}
+}
+
--- server/server.go
+++ server/server.go
@@ -17,8 +17,10 @@ func main() {
 		port = defaultPort
 	}
 
+	c := graphql.Config{Resolvers: &graphql.Resolver{}}
+
 	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(graphql.Config{Resolvers: &graphql.Resolver{}})))
+	http.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(c)))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
